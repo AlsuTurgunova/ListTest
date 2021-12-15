87ad44d2d90886b5dd71f05306061923c3884ef3
AlsuTurgunova <92831826+AlsuTurgunova@users.noreply.github.com>
2021-12-15 08:15:39 +0300

    version

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"operation","l":"ListTest"},{"p":"operation","l":"ListTestTest"},{"p":"operation","l":"main"}];updateSearchResults();
